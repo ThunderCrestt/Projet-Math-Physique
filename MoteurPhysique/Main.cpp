5bd52b869c0f0c32bf1ade17577a5314e7301575
Nico <nicey74300@gmail.com>
2021-09-26 15:30:15 -0400

    Ajout récupération des coordonnées lors d'un clic gauche

--- MoteurPhysique/Main.cpp
+++ MoteurPhysique/Main.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include "Particule.h"
 
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		particulCount++;
+		double xpos, ypos;
+		//getting cursor position
+		glfwGetCursorPos(window, &xpos, &ypos);
+		std::cout << "Cursor Position at (" << xpos << " : " << ypos << std::endl;
+		
+	}
+
+}
+int particulCount=0;
 
 int main()
 {
+	
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -18,14 +35,20 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+	
 	glfwMakeContextCurrent(window);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+		//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		glfwSetMouseButtonCallback(window, mouse_button_callback);
+
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
+
