c715e159b6e98e3e787536e2a72e7c647ddb9b59
Pierre <pierresourdeau@hotmail.fr>
2021-09-27 19:59:08 -0400

    ajout des trajectoires sur les projectiles
    fonctionnement des trajectoires & des callback

--- MoteurPhysique/Main.cpp
+++ MoteurPhysique/Main.cpp
@@ -26,8 +26,8 @@ const int NumObjects = 2;
 const int iLines = 0;
 const int iTriangles = 1;
 const int num_segments = 100;
-float circleCenterX = 0.5f;
-float circleCenterY = 0.5f;
+float circleCenterX = -0.5;
+float circleCenterY = -0.5;
 //ParticuleSystem
 //Particule
 unsigned int myVBO[NumObjects];  
@@ -62,6 +62,7 @@ void setupGeometries() {
 		-0.4f,  0.0f, 0.0f,		1.0f, 0.8f, 0.8f
 
 	};
+	/*
 	glBindVertexArray(myVAO[iTriangles]);
 	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iTriangles]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(trianglesVerts), trianglesVerts, GL_STATIC_DRAW);
@@ -69,7 +70,7 @@ void setupGeometries() {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
+	*/
 	//deuxieme figure : ligne
 	//std::vector<float> sixVertsForLines = std::vector<float> ();
 	float sixVertsForLines[2 * num_segments];
@@ -79,8 +80,8 @@ void setupGeometries() {
 	for (int ii = 0; ii < num_segments; ii++)
 	{
 		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = 0.5 * cosf(theta);
-		float y = 0.5 * sinf(theta);
+		float x = 0.1 * cosf(theta);
+		float y = 0.1 * sinf(theta);
 		sixVertsForLines[posX] = x + circleCenterX;
 		sixVertsForLines[posY] = y + circleCenterY;
 		posX += 2;
@@ -106,9 +107,10 @@ void rendScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgram);
 	//dessin des triangles
+	/*
 	glBindVertexArray(myVAO[iTriangles]);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
-
+	*/
 	//dessin du crecle
 	glBindVertexArray(myVAO[iLines]);
 	glVertexAttrib3f(1, 1.0f, 1.0f, 0.2f);		// A yellow-ish color (R, G, B values).
@@ -118,13 +120,13 @@ void rendScene() {
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	Particule *particule = reinterpret_cast<Particule*>(glfwGetWindowUserPointer(window));
-	Vector3D basePosition = Vector3D(0, 0, 0);
-	Vector3D initialSpeed = Vector3D(0.07, 0.07, 0);
+	Vector3D basePosition = Vector3D(-0.5, -0.5, 0);
+	Vector3D initialSpeed = Vector3D(0.9, 0.9, 0);
 	Vector3D acceleration;
 	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
 	{
 		//update la particule
-		acceleration = Vector3D(0, -9.81, 0);
+		acceleration = Vector3D(0, -0.981, 0);
 		particule->setupVectors(basePosition, initialSpeed, acceleration);
 		particule->setMass(25);
 		//update la couleur
@@ -133,7 +135,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 	else if(key == GLFW_KEY_2 && action == GLFW_PRESS) {
 		//update la particule
-		acceleration = Vector3D(0, -12, 0);
+		acceleration = Vector3D(0, -1.2, 0);
 		particule->setupVectors(basePosition, initialSpeed, acceleration);
 		particule->setMass(75);
 		//update la couleur
@@ -142,7 +144,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 	else if (key == GLFW_KEY_3 && action == GLFW_PRESS) {
 		//update la particule
-		acceleration = Vector3D(0, -15, 0);
+		acceleration = Vector3D(0, -1.5, 0);
 		particule->setupVectors(basePosition, initialSpeed, acceleration);
 		particule->setMass(130);
 		//update la couleur
@@ -156,9 +158,9 @@ int main()
 	// initialisation de la fen�tre d'openGL
 	//initialisation d'une particule qui sera modifié pour chaque type de projectile
 	ParticuleSystem system = ParticuleSystem();
-	Vector3D position = Vector3D(1, 0, 0);
-	Vector3D initialSpeed = Vector3D(0.07, 0.07, 0);
-	Vector3D acceleration =Vector3D(0,-9.81,0); //acc must be different for each object
+	Vector3D position = Vector3D(-0.5, -0.5, 0);
+	Vector3D initialSpeed = Vector3D(0.7, 0.7, 0);
+	Vector3D acceleration =Vector3D(0,-0.981,0); //acc must be different for each object
 	Particule bouboule = Particule(10, 1, position, initialSpeed, acceleration,system);
 	glfwInit();
 
@@ -166,7 +168,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1000, 1000, "test", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create a GLFW window" << std::endl;
@@ -228,6 +230,7 @@ int main()
 		//for now
 		circleCenterX = bouboule.getPosition().getX();
 		circleCenterY = bouboule.getPosition().getY();
+		setupGeometries();
 		rendScene();
 
 
