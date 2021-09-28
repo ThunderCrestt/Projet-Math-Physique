6826a9ab18655a7dfd9479d6fb1553dfea2a33ce
Mona <m6lecoz@enib.fr>
2021-09-28 15:49:52 -0400

    Ajout du rayon variable et texte

--- MoteurPhysique/Main.cpp
+++ MoteurPhysique/Main.cpp
@@ -5,8 +5,12 @@
 #include "Particule.h"
 #include "ParticuleSystem.h"
 #include<vector>
+#include"Libraries/include/ft2build.h"
+#include FT_FREETYPE_H  
+#include <stdlib.h>
 
 
+// vertex shader basique
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
@@ -14,30 +18,24 @@ const char* vertexShaderSource = "#version 330 core\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
+// fragment shader simple avec couleur gris clair
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+"   FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f);"
 "}\0";
 
 unsigned int shaderProgram;
-const int NumObjects = 2;
-const int iLines = 0;
-const int iTriangles = 1;
 const int num_segments = 100;
 float circleCenterX = -0.5;
 float circleCenterY = -0.5;
+float rCircle = 0.1;
 //ParticuleSystem
 //Particule
-unsigned int myVBO[NumObjects];  
-unsigned int myVAO[NumObjects];
-
-// r�ajuste viewport
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
+unsigned int myVBO;  
+unsigned int myVAO;
+FT_Face face;
 
 
 void processInput(GLFWwindow* window)
@@ -48,31 +46,10 @@ void processInput(GLFWwindow* window)
 
 // recuperation des vertices pour dessiner plus tard 
 void setupGeometries() {
-	glGenVertexArrays(NumObjects, &myVAO[0]);
-	glGenBuffers(NumObjects, &myVBO[0]);
+	glGenVertexArrays(1, &myVAO);
+	glGenBuffers(1, &myVBO);
 
-	//Premi�re figure : triangles
-	float trianglesVerts[] = {		//color
-		-0.8f, -0.2f, 0.0f,		1.0f, 0.8f, 0.8f,		//premier triangle (forme la moiti� du canon)
-		-0.4f, -0.2f, 0.0f,		1.0f, 0.8f, 0.8f,
-		-0.4f,  0.0f, 0.0f,		1.0f, 0.8f, 0.8f,
-
-		-0.8f, -0.2f, 0.0f,		1.0f, 0.8f, 0.8f,		//deuxieme triangle
-		-0.8f, 0.0f, 0.0f,		1.0f, 0.8f, 0.8f,
-		-0.4f,  0.0f, 0.0f,		1.0f, 0.8f, 0.8f
-
-	};
-	/*
-	glBindVertexArray(myVAO[iTriangles]);
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iTriangles]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(trianglesVerts), trianglesVerts, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	*/
-	//deuxieme figure : ligne
-	//std::vector<float> sixVertsForLines = std::vector<float> ();
+	//figure cercle : utilisation de LINE_LOOP
 	float sixVertsForLines[2 * num_segments];
 	int posX = 0;
 	int posY = 1;
@@ -80,47 +57,44 @@ void setupGeometries() {
 	for (int ii = 0; ii < num_segments; ii++)
 	{
 		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = 0.1 * cosf(theta);
-		float y = 0.1 * sinf(theta);
+		float x = rCircle * cosf(theta);
+		float y = rCircle * sinf(theta);
 		sixVertsForLines[posX] = x + circleCenterX;
 		sixVertsForLines[posY] = y + circleCenterY;
 		posX += 2;
 		posY += 2;
 	}
 
-
-	glBindVertexArray(myVAO[iLines]);
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iLines]);
+	//Conserve les vertices des segments du cercle pour les dessiner plus tard 
+	glBindVertexArray(myVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, myVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(sixVertsForLines), &sixVertsForLines, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(0);
-
-
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
 
 
-// dessine les formes en fonction des vertices pass�s dans myVAO
+// dessine les formes en fonction des vertices passés dans myVAO
 void rendScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgram);
-	//dessin des triangles
-	/*
-	glBindVertexArray(myVAO[iTriangles]);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	*/
+
 	//dessin du crecle
-	glBindVertexArray(myVAO[iLines]);
+	glBindVertexArray(myVAO);
 	glVertexAttrib3f(1, 1.0f, 1.0f, 0.2f);		// A yellow-ish color (R, G, B values).
 	glDrawArrays(GL_LINE_LOOP, 0, num_segments);
+
+	
+	
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	Particule *particule = reinterpret_cast<Particule*>(glfwGetWindowUserPointer(window));
-	Vector3D basePosition = Vector3D(-0.5, -0.5, 0);
+	Vector3D basePosition = Vector3D(-0.5, 0, 0);
 	Vector3D initialSpeed = Vector3D(0.9, 0.9, 0);
 	Vector3D acceleration;
 	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
@@ -131,7 +105,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		particule->setMass(25);
 		//update la couleur
 		//update le rayon
+		rCircle = 0.1;
 		//setupGeometric
+		system("cls");
+		std::wcout << "Projectile : boule de feu";
 	}
 	else if(key == GLFW_KEY_2 && action == GLFW_PRESS) {
 		//update la particule
@@ -140,7 +117,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		particule->setMass(75);
 		//update la couleur
 		//update le rayon
+		 rCircle = 0.15;
 		//setupGeometric
+		 system("cls");
+		 std::wcout << "Projectile : ballon";
 	}
 	else if (key == GLFW_KEY_3 && action == GLFW_PRESS) {
 		//update la particule
@@ -149,7 +129,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		particule->setMass(130);
 		//update la couleur
 		//update le rayon
+		 rCircle = 0.2;
 		//setupGeometric
+		 std::system("cls");
+		 std::wcout << "Projectile : boulet de canon";
 	}
 }
 
@@ -158,7 +141,7 @@ int main()
 	// initialisation de la fen�tre d'openGL
 	//initialisation d'une particule qui sera modifié pour chaque type de projectile
 	ParticuleSystem system = ParticuleSystem();
-	Vector3D position = Vector3D(-0.5, -0.5, 0);
+	Vector3D position = Vector3D(-0.5, 0.0, 0);
 	Vector3D initialSpeed = Vector3D(0.7, 0.7, 0);
 	Vector3D acceleration =Vector3D(0,-0.981,0); //acc must be different for each object
 	Particule bouboule = Particule(10, 1, position, initialSpeed, acceleration,system);
@@ -168,7 +151,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1000, 1000, "test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1200, 800, "test", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create a GLFW window" << std::endl;
@@ -209,11 +192,16 @@ int main()
 
 	//affichage de la fen�tre
 
-	glViewport(0, 0, 800, 800);
-	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glViewport(0, 0, 1000, 1000);
 
 	setupGeometries();
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	std::system("cls");
+	std::wcout << "Projectile : boule de feu";
+
 
 	//boucle de jeu
 	double currentFrame = glfwGetTime();
