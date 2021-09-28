9849e4d9c8b1184773f4fdddc05f275bd937455f
Mona <m6lecoz@enib.fr>
2021-09-27 20:04:18 -0400

    Modifications shaders

--- MoteurPhysique/Main.cpp
+++ MoteurPhysique/Main.cpp
@@ -9,23 +9,27 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	vertexColor = vec4(0.9, 0.9, 0.9, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.2f, 0.2f, 1.0f);"
+"   FragColor = vertexColor;"
 "}\0";
+
 
 unsigned int shaderProgram;
 const int NumObjects = 2;
 const int iLines = 0;
 const int iTriangles = 1;
-const int num_segments = 100;
+const int num_segments = 80;
 const float circleCenterX = -0.7f;
 const float circleCenterY = 0.0f;
 float colorCircle[] = { 1, 1.0f, 1.0f, 1.0f };
@@ -98,13 +102,10 @@ void setupGeometries(float r) {
 void rendScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgram);
-	//dessin des triangles
-	glBindVertexArray(myVAO[iTriangles]);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	
 
 	//dessin du crecle
 	glBindVertexArray(myVAO[iLines]);
-	glVertexAttrib3f(1, 0.0f, 0.0f, 0.0f);		// A yellow-ish color (R, G, B values).
 	glDrawArrays(GL_LINE_LOOP, 0, num_segments);
 
 
@@ -173,10 +174,10 @@ int main()
 
 	glViewport(0, 0, 800, 800);
 
-	setupGeometries(0.2);
+	setupGeometries(0.1);
 
-	//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	//glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	//boucle de jeu
 	while (!glfwWindowShouldClose(window))
