397941b2799ec7a9a2a70361d0d5572b58d7342f
Pierre <pierresourdeau@hotmail.fr>
2021-10-26 17:33:01 -0400

    ajout force flottaison

--- MoteurPhysique/ParticuleFlottaison.cpp
+++ MoteurPhysique/ParticuleFlottaison.cpp
@@ -1,1 +1,25 @@
 #include "ParticuleFlottaison.h"
+ParticuleFlottaison::ParticuleFlottaison(float profondeurMax, float volume, float hauteurEau, float densité)
+{
+	this->_profondeurMax = profondeurMax;
+	this->_volume = volume;
+	this->_hauteurEau = hauteurEau;
+	this->_densité = densité;
+}
+void ParticuleFlottaison::updateForce(Particule* particule, float duration)
+{
+	float profondeur = particule->getPosition().getY();
+	// on regarde si on est pas hors de l'eau
+	if (profondeur >= _hauteurEau + _profondeurMax) return;
+	Vector3D force(0, 0, 0);
+	// on va voir si on est pas à la profondeur max
+	if (profondeur <= _hauteurEau - _profondeurMax)
+	{
+		force.setY(_densité * _volume);
+		particule->addForce(force);
+		return;
+	}
+	//Sinon on est juste partiellement submergé
+	force.setY(_densité * _volume *(profondeur - _profondeurMax - _hauteurEau) / 2 * _profondeurMax);
+	particule->addForce(force);
+}
--- MoteurPhysique/ParticuleFlottaison.h
+++ MoteurPhysique/ParticuleFlottaison.h
@@ -1,5 +1,16 @@
 #pragma once
-class ParticuleFlottaison
+#include "ParticuleForceGenerator.h"
+#include "Vector3D.h"
+class ParticuleFlottaison : public ParticuleForceGenerator
 {
+private:
+	float _profondeurMax;
+	float _volume;
+	float _hauteurEau;
+	float _densité;
+
+public:
+	ParticuleFlottaison(float profondeurMax,float volume,float hauteurEau,float densité);
+	void updateForce(Particule* particule, float duration);
 };
 
