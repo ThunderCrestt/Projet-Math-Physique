017b2bd4b406e365878e3a1254917a0f5148f599
Mona <m6lecoz@enib.fr>
2021-11-18 20:39:41 -0500

    Fusion travaux

--- MoteurPhysique/Main.cpp
+++ MoteurPhysique/Main.cpp
@@ -12,6 +12,7 @@
 #include "PlaneSurface2DContactGenerator.h"
 #include "ParticuleCable.h"
 #include "ParticuleElastique.h"
+#include "Matrix3.h"
 
 // vertex shader basique
 const char* vertexShaderSource = "#version 330 core\n"
@@ -26,7 +27,7 @@ const char* fragmentShaderSourceGrey = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.6f, 0.6f, 0.6f, 1.0f);"
+"   FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f);"
 "}\0";
 
 // fragment shader simple avec couleur verte
@@ -40,15 +41,13 @@ const char* fragmentShaderSourceGreen = "#version 330 core\n"
 unsigned int shaderProgramGrey;
 unsigned int shaderProgramGreen;
 const int num_segments = 100;
-const int iCircle = 0;
-const int iFirstSquare = 1;
-const int iSecondSquare = 2;
-const int iThirdSquare = 3;
-float circleCenterX = -0.5;
-float circleCenterY = 0.5;
-float rCircle = 0.03;
-unsigned int myVBO[4];
-unsigned int myVAO[4];
+const int iFirstSquare = 0;
+const int iSecondSquare = 1;
+const int iPoint = 2;
+unsigned int myVBO[3];
+unsigned int myVAO[3];
+Vector3D gravityCenter = Vector3D(0.0, 0.0, 0);
+float transformMatrice[] = { 0,-1,1,0 };
 
 
 
@@ -60,106 +59,59 @@ void processInput(GLFWwindow* window)
 
 // recuperation des vertices pour dessiner plus tard 
 void setupGeometries(ParticuleWorld system) {
-	glGenVertexArrays(4, &myVAO[0]);
-	glGenBuffers(4, &myVBO[0]);
+	glGenVertexArrays(3, &myVAO[0]);
+	glGenBuffers(3, &myVBO[0]);
 
-	//figure cercle pour les blobs
-	float sixVertsForLines[10 * num_segments];
-	int posX = 0;
-	int posY = 1;
-	Registre registre = system.getRegistry().getRegistre();
+	
+	//enregistrement des vertexes de la Figure
 
-	//Injection des points du premier blob (blob central)
-	for (int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = rCircle * cosf(theta);
-		float y = rCircle * sinf(theta);
-		sixVertsForLines[posX] = x + system.getRegistry().getEnregistrementAtPos(0).particule->getPosition().getX();
-		sixVertsForLines[posY] = y + system.getRegistry().getEnregistrementAtPos(0).particule->getPosition().getY();
-		posX += 2;
-		posY += 2;
-	}
+	//Création des 6 points de notre figure
+	Vector3D vertex1 = { (float)(gravityCenter.getX() - 0.2), (float)(gravityCenter.getY() - 0.2) , 0 };
+	Vector3D vertex2 = { (float)(gravityCenter.getX() - 0.2), (float)(gravityCenter.getY() + 0.2) , 0 };
+	Vector3D vertex3 = { (float)(gravityCenter.getX() + 0.2), (float)(gravityCenter.getY() + 0.2) , 0 };
+	Vector3D vertex4 = { (float)(gravityCenter.getX() + 0.2), (float)(gravityCenter.getY() - 0.2) , 0 };
+	Vector3D vertex5 = { (float)(gravityCenter.getX() + 0.3), (float)(gravityCenter.getY() + 0.2) , 0 };
+	Vector3D vertex6 = { (float)(gravityCenter.getX() + 0.3), (float)(gravityCenter.getY() + 0.1) , 0 };
+	Vector3D vertex7 = { (float)(gravityCenter.getX() + 0.2), (float)(gravityCenter.getY() + 0.1) , 0 };
 
-	//Deuxième blob
-	for (int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = rCircle * cosf(theta);
-		float y = rCircle * sinf(theta);
-		sixVertsForLines[posX] = x + system.getRegistry().getEnregistrementAtPos(1).particule->getPosition().getX();
-		sixVertsForLines[posY] = y + system.getRegistry().getEnregistrementAtPos(1).particule->getPosition().getY();
-		posX += 2;
-		posY += 2;
-	}
+	// transformation de tous nos points
 
-	//Troisième blob
-	for (int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = rCircle * cosf(theta);
-		float y = rCircle * sinf(theta);
-		sixVertsForLines[posX] = x + system.getRegistry().getEnregistrementAtPos(2).particule->getPosition().getX();
-		sixVertsForLines[posY] = y + system.getRegistry().getEnregistrementAtPos(2).particule->getPosition().getY();
-		posX += 2;
-		posY += 2;
-	}
+	Matrix3 transform = Matrix3({ {{(float)(sqrt(2)/2),(float)(-sqrt(2) / 2),0},
+									{(float)(sqrt(2) / 2),(float)(sqrt(2) / 2),0},
+									{0,0,1}} });
 
-	//Quatrième blob
-	for (int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = rCircle * cosf(theta);
-		float y = rCircle * sinf(theta);
-		sixVertsForLines[posX] = x + system.getRegistry().getEnregistrementAtPos(3).particule->getPosition().getX();
-		sixVertsForLines[posY] = y + system.getRegistry().getEnregistrementAtPos(3).particule->getPosition().getY();
-		posX += 2;
-		posY += 2;
-	}
+	vertex1 = transform * vertex1;
+	vertex2 = transform * vertex2;
+	vertex3 = transform * vertex3;
+	vertex4 = transform * vertex4;
+	vertex5 = transform * vertex5;
+	vertex6 = transform * vertex6;
+	vertex7 = transform * vertex7;
 
-	//Cinquième blob
-	for (int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = rCircle * cosf(theta);
-		float y = rCircle * sinf(theta);
-		sixVertsForLines[posX] = x + system.getRegistry().getEnregistrementAtPos(4).particule->getPosition().getX();
-		sixVertsForLines[posY] = y + system.getRegistry().getEnregistrementAtPos(4).particule->getPosition().getY();
-		posX += 2;
-		posY += 2;
-	}
-
-	//Conservation de tout les cercles
-	glBindVertexArray(myVAO[iCircle]);
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iCircle]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(sixVertsForLines), &sixVertsForLines, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(0);
-
-
-	// figure première plateforme
+	//Carré 1
 	float firstSquare[] = {
-		-1.0f, -0.2f,
-		-0.3f, -0.2f,
-		-0.3f, -1.0f,
-		-1.0f, -0.2f,
-		-1.0f, -1.0f,
-		-0.3f, -1.0f,
+		vertex1.getX(),vertex1.getY(),
+		vertex2.getX(),vertex2.getY(),
+		vertex3.getX(),vertex3.getY(),
+		vertex1.getX(),vertex1.getY(),
+		vertex4.getX(),vertex4.getY(),
+		vertex3.getX(),vertex3.getY(),
 	};
+
 	glBindVertexArray(myVAO[iFirstSquare]);
 	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iFirstSquare]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(firstSquare), firstSquare, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//Figure deuxième plateforme
+	//Carré 2
 	float secondSquare[] = {
-		-0.3f, -1.0f,
-		0.3f, -1.0f,
-		0.3f, -0.5f,
-		-0.3f, -1.0f,
-		-0.3f, -0.5f,
-		0.3f, -0.5f,
+		vertex7.getX(),vertex7.getY(),
+		vertex2.getX(),vertex2.getY(),
+		vertex5.getX(),vertex5.getY(),
+		vertex7.getX(),vertex7.getY(),
+		vertex6.getX(),vertex6.getY(),
+		vertex5.getX(),vertex5.getY(),
 	};
 	glBindVertexArray(myVAO[iSecondSquare]);
 	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iSecondSquare]);
@@ -167,23 +119,16 @@ void setupGeometries(ParticuleWorld system) {
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//Figure troisième plateforme
-	float thirdSquare[] = {
-		0.3f, -0.7f,
-		1.0f, -0.7f,
-		1.0f, -1.0f,
-		0.3f, -0.7f,
-		0.3f, -1.0f,
-		1.0f, -1.0f,
+	//point de gravité
+	float pointVertex[] = {
+		gravityCenter.getX(), gravityCenter.getY(),
 	};
-	glBindVertexArray(myVAO[iThirdSquare]);
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iThirdSquare]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(thirdSquare), thirdSquare, GL_STATIC_DRAW);
+	glBindVertexArray(myVAO[iPoint]);
+	glBindBuffer(GL_ARRAY_BUFFER, myVBO[iPoint]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pointVertex), pointVertex, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 }
 
 
@@ -192,16 +137,6 @@ void rendScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgramGreen);
 
-	//dessin des blobs:
-	glBindVertexArray(myVAO[iCircle]);
-	glDrawArrays(GL_LINE_LOOP, 0, num_segments);
-	glDrawArrays(GL_LINE_LOOP, num_segments, num_segments);
-	glDrawArrays(GL_LINE_LOOP, 2 * num_segments, num_segments);
-	glDrawArrays(GL_LINE_LOOP, 3 * num_segments, num_segments);
-	glDrawArrays(GL_LINE_LOOP, 4 * num_segments, num_segments);
-
-	glUseProgram(shaderProgramGrey);
-
 	//Dessin des plateforme:
 	glBindVertexArray(myVAO[iFirstSquare]);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -209,9 +144,11 @@ void rendScene() {
 	glBindVertexArray(myVAO[iSecondSquare]);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 
-	glBindVertexArray(myVAO[iThirdSquare]);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glUseProgram(shaderProgramGrey);
 
+	//dessin point de gravité
+	glBindVertexArray(myVAO[iPoint]);
+	glDrawArrays(GL_POINTS, 0, 1);
 }
 
 //Récuperation des évenements clavier
@@ -249,73 +186,7 @@ int main()
 	//ParticuleContactResolver resolver=ParticuleContactResolver(iterationsContactResolver);
 	ParticuleWorld system = ParticuleWorld();
 	system.resolver.setIterations(15);
-	Vector3D positionB1 = Vector3D(-0.5, 0.5, 0);
-	Vector3D positionB2 = Vector3D(-0.43, 0.5, 0);
-	Vector3D positionB3 = Vector3D(-0.57, 0.5, 0);
-	Vector3D positionB4 = Vector3D(-0.5, 0.57, 0);
-	Vector3D positionB5 = Vector3D(-0.5, 0.43, 0);
-	//Vector3D initialSpeed = Vector3D(0.7, 0.7, 0);
-	//Vector3D acceleration =Vector3D(0,-0.981,0); //acc must be different for each object
-	Particule blob1 = Particule(1, 1, positionB1, 0, 0,rCircle);
-	Particule blob2 = Particule(1, 1, positionB2, 0, 0, rCircle);
-	Particule blob3 = Particule(1, 1, positionB3, 0, 0, rCircle);
-	Particule blob4 = Particule(1, 1, positionB4, 0, 0, rCircle);
-	Particule blob5 = Particule(1, 1, positionB5, 0, 0, rCircle);
-	//les particules doivent être ajouter au système aussi
-	system.addParticule(blob1);
-	system.addParticule(blob2);
-	system.addParticule(blob3);
-	system.addParticule(blob4);
-	system.addParticule(blob5);
 
-
-	//Ajout de la premiere particule avec un generateur de force gravité
-	Vector3D gravity = Vector3D(0, -0.9, 0);
-	GravityForceGenerator gravityGenerator = GravityForceGenerator(gravity);
-	system.addToRegistreForce(blob1, gravityGenerator);
-	system.addToRegistreForce(blob2, gravityGenerator);
-	system.addToRegistreForce(blob3, gravityGenerator);
-	system.addToRegistreForce(blob4, gravityGenerator);
-	system.addToRegistreForce(blob5, gravityGenerator);
-
-	//Ajout des ressort entre le blob 1 et les autres
-
-	ParticuleSpring ressortGeneratorb1b2 = ParticuleSpring(&blob2, 3, 0.07);
-	system.addToRegistreForce(blob1, ressortGeneratorb1b2);
-	ParticuleSpring ressortGeneratorb1b3 = ParticuleSpring(&blob3, 3, 0.07);
-	system.addToRegistreForce(blob1, ressortGeneratorb1b3);
-	ParticuleSpring ressortGeneratorb1b4 = ParticuleSpring(&blob4, 3, 0.07);
-	system.addToRegistreForce(blob1, ressortGeneratorb1b4);
-	ParticuleSpring ressortGeneratorb1b5 = ParticuleSpring(&blob5, 3, 0.07);
-	system.addToRegistreForce(blob1, ressortGeneratorb1b5);
-	ParticuleSpring ressortGeneratorb2b1 = ParticuleSpring(&blob1, 3, 0.07);
-	system.addToRegistreForce(blob2, ressortGeneratorb2b1);
-	ParticuleSpring ressortGeneratorb3b1 = ParticuleSpring(&blob1, 3, 0.07);
-	system.addToRegistreForce(blob3, ressortGeneratorb3b1);
-	ParticuleSpring ressortGeneratorb4b1 = ParticuleSpring(&blob1, 3, 0.07);
-	system.addToRegistreForce(blob4, ressortGeneratorb4b1);
-	ParticuleSpring ressortGeneratorb5b1 = ParticuleSpring(&blob1, 3, 0.07);
-	system.addToRegistreForce(blob5, ressortGeneratorb5b1);
-	
-	//ajout des divers surfaces
-
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(-0.8, 0.9, 0), Vector3D(-0.8, -0.9, 0), system.getAllParticules(), 0));
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(0.8, -0.9, 0), Vector3D(0.8, 0.9, 0), system.getAllParticules(), 0));
-
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(-1.3,-0.2,0),Vector3D(-0.3,-0.2,0),system.getAllParticules(),0.2));
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(-0.3,-0.2,0),Vector3D(-0.3,-0.6,0),system.getAllParticules(),0.2));
-
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(-0.4, -0.5, 0), Vector3D(0.3, -0.5, 0), system.getAllParticules(),0.2));
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(0.3, -0.5, 0), Vector3D(0.3, -0.8, 0), system.getAllParticules(),0.2));
-
-	system.registreContactGenerator.push_back(new PlaneSurface2DContactGenerator(Vector3D(0.2, -0.7, 0), Vector3D(1.2, -0.7, 0), system.getAllParticules(),0.2));
-
-	
-	//ajout des cables entres les particules
-	system.registreContactGenerator.push_back(new ParticuleCable(&blob1, &blob2, 0.09, 0.1));
-	system.registreContactGenerator.push_back(new ParticuleCable(&blob1, &blob3, 0.09, 0.1));
-	system.registreContactGenerator.push_back(new ParticuleCable(&blob1, &blob4, 0.09, 0.1));
-	system.registreContactGenerator.push_back(new ParticuleCable(&blob1, &blob5, 0.09, 0.1));
 	
 	// initialisation de la fen�tre d'openGL
 	glfwInit();
@@ -332,7 +203,6 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSetWindowUserPointer(window, &blob1);
 	glfwSetKeyCallback(window, key_callback);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -390,11 +260,11 @@ int main()
 	double deltaTime;
 	while (!glfwWindowShouldClose(window))
 	{
-		currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		system.runPhysic(deltaTime);
-		processInput(window);
+		//currentFrame = glfwGetTime();
+		//deltaTime = currentFrame - lastFrame;
+		//lastFrame = currentFrame;
+		//system.runPhysic(deltaTime);
+		//processInput(window);
 		setupGeometries(system);
 		rendScene();
 		//std::cout << system.getAllParticules()[0]->getPosition().getX()<<std::endl;
