50c7c8740d5ae015bb29ef84d1137f3fea2bfeb1
Pierre <pierresourdeau@hotmail.fr>
2021-11-22 18:55:30 -0500

    début quaternion

--- MoteurPhysique/Quaternion.cpp
+++ MoteurPhysique/Quaternion.cpp
@@ -1,1 +1,16 @@
 #include "Quaternion.h"
+
+void Quaternion::normalize()
+{
+	float d = r * r + i * i + j * j + k * k;
+	if (d == 0)
+	{
+		r = 1;
+		return;
+	}
+	d = ((float)1.0) / (float)sqrt(d);
+	r *= d;
+	i *= d;
+	j *= d;
+	k *= d;
+}
--- MoteurPhysique/Quaternion.h
+++ MoteurPhysique/Quaternion.h
@@ -1,8 +1,43 @@
 #pragma once
-#include "Matrix4.h"
+#include <array>
 class Quaternion
 {
-	private:
-	Matrix4 matrix;
+
+public:
+	union {
+		struct {
+			/**
+			*Contient la composante réel du quaternion
+			*/
+			float r;
+			/**
+			* Contient la première composante complexe du quaternion
+			*/
+			float i;
+			/**
+			* Contient la deuxième composante complexe du quaternion.
+			*/
+			float j;
+				/**
+				* Contient la troisème composante complexe du quaternion
+				*/
+			float k;
+		};
+		/**
+		* Holds the quaternion data in array form.
+		*/
+		std::array<float,4> data;
+	};
+	Quaternion(float r, float i, float j, float k)
+	{
+		this->r = r;
+		this->i = i;
+		this->j = j;
+		this->k = k;
+
+	}
+	void normalize();
+
 };
+
 
